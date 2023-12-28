e4d7e3d386eac09dcf248971df6abfa8d6d87e99
Isaac Lopez <isaac.lopez@upr.edu>
2023-12-28 14:56:50 -0400

    added links w song

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Codec.Binary.UTF8.String (utf8Encode)
-import Data.Aeson
+import Data.Aeson hiding (encode)
+import Data.Foldable (traverse_)
 import Data.List (intercalate)
 import GHC.Generics (Generic)
 import Network.HTTP.Simple
@@ -22,17 +23,26 @@ data Song = Song
     , category :: String
     , imdb :: String
     }
-    deriving (Show, Generic, FromJSON, ToJSON)
+    deriving (Show, Generic)
 
+instance ToJSON Song
+instance FromJSON Song
+
+-- TPB api link
 api :: String
 api = "https://apibay.org/"
 
-torrentLink :: String
-torrentLink = undefined
+-- function to make our torrrent link
+torrentLink :: Song -> String
+torrentLink Song{name, info_hash} = "magnet:?xt=urn:btih:" ++ info_hash ++ "&dn=" ++ name' ++ "&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.bittor.pw%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fpublic.popcorn-tracker.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce"
+  where
+    name' = encodeTitle name
 
+-- encode song/audio title to URI utf8
 encodeTitle :: String -> String
 encodeTitle = encode . utf8Encode
 
+-- Search on API
 search :: String -> String
 search (words -> s) = "q.php?q=" <> s' <> "&cat=101"
   where
@@ -42,5 +52,6 @@ main :: IO ()
 main = do
     l <- getLine
     req <- parseRequest ("GET " <> api <> search l)
-    resp <- httpBS req
-    putStrLn $ unpack (getResponseBody resp)
+    resp <- httpJSON req :: IO (Response [Song])
+    let ls = getResponseBody resp
+    traverse_ (\x -> putStrLn (name x ++ ": " ++ torrentLink x)) ls
