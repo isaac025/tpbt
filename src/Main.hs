f1257c05ff36089009bc749b9cef1b3c0702fd62
Isaac Lopez <isaac.lopez@upr.edu>
2024-04-04 16:36:58 -0400

    adding more views

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
 import Brick.AttrMap (AttrMap, AttrName, attrMap, attrName)
+import Brick.Focus (FocusRing, focusNext, focusPrev, focusRing, focusSetCurrent, withFocusRing)
 import Brick.Main (App (..), defaultMain, halt, showFirstCursor)
-import Brick.Types (BrickEvent (VtyEvent), EventM, Widget)
+import Brick.Types (BrickEvent (VtyEvent), EventM, Widget, zoom)
 import Brick.Util (fg)
 import Brick.Widgets.Border (border)
 import Brick.Widgets.Border.Style (unicodeBold)
@@ -15,7 +18,9 @@ import Brick.Widgets.Edit (
     handleEditorEvent,
     renderEditor,
  )
+import Brick.Widgets.List (List, list)
 import Control.Monad (void)
+import Data.Vector (empty)
 import Graphics.Vty.Attributes (
     bold,
     brightBlue,
@@ -25,16 +30,27 @@ import Graphics.Vty.Attributes (
     withStyle,
  )
 import Graphics.Vty.Input (Event (EvKey), Key (..))
-import Lens.Micro ((&))
+import Lens.Micro ((&), (^.))
+import Lens.Micro.Mtl ((%=))
+import Lens.Micro.TH (makeLenses)
+import TPB
 
 data Name = Search | ResultList | ContentList
     deriving (Eq, Ord, Show)
 
-ui :: Editor String () -> [Widget ()]
+data State = State
+    { _focus :: FocusRing Name
+    , _results :: List Name Result
+    , _contents :: List Name Content
+    , _searchField :: Editor String Name
+    }
+makeLenses ''State
+
+ui :: State -> [Widget Name]
 ui s = [vCenter $ maxWidth 200 $ vBox [drawSearch, instructions]]
   where
     drawSearch = withBorderStyle unicodeBold $ border $ vLimit 1 $ label <+> e
-    e = renderEditor (str . unlines) True s
+    e = renderEditor (str . unlines) True (s ^. searchField)
     label = str "Search: "
     maxWidth w = hCenter . hLimit w
     instructions =
@@ -45,20 +61,33 @@ ui s = [vCenter $ maxWidth 200 $ vBox [drawSearch, instructions]]
                 , drawInstr "/" "back to search"
                 ]
 
-drawInstr :: String -> String -> Widget ()
+drawList :: List Name Result -> Widget Name
+drawList l = listUi
+  where
+    label = str "Results:"
+    box = borderWithLabel
+drawInstr :: String -> String -> Widget Name
 drawInstr k a =
     withAttr attrKey (str k)
         <+> str " to "
         <+> withAttr attrBold (str a)
         & hCenter
 
-initialState :: Editor String ()
-initialState = editor () Nothing ""
+initialState :: State
+initialState =
+    State
+        { _focus = focusRing [Search, ResultList, ContentList]
+        , _results = list ResultList empty 1
+        , _contents = list ContentList empty 1
+        , _searchField = editor Search Nothing ""
+        }
 
-event :: BrickEvent () e -> EventM () (Editor String ()) ()
-event (VtyEvent (EvKey (KChar 'q') [])) = halt
+event :: BrickEvent Name e -> EventM Name State ()
 event (VtyEvent (EvKey KEsc [])) = halt
-event v = handleEditorEvent v
+event (VtyEvent (EvKey (KChar '\t') [])) = focus %= focusNext
+event (VtyEvent (EvKey (KChar '/') [])) = focus %= focusSetCurrent Search
+event (VtyEvent (EvKey KBackTab [])) = focus %= focusPrev
+event v = zoom searchField $ handleEditorEvent v
 
 attrBold :: AttrName
 attrBold = attrName "bold"
@@ -74,7 +103,7 @@ theme =
         , (attrKey, withStyle (fg brightBlue) bold)
         ]
 
-app :: App (Editor String ()) e ()
+app :: App State e Name
 app =
     App
         { appDraw = ui
